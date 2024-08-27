48540e646b962e4e398017aaa3d880a8040dd0fa
MadushanaKasilingam <madushanakasilingam003@gmail.com>
2024-08-27 23:26:00 +0530

    Commit

---
+++ src/main/java/com/project/restaurantsystem/model/Address.java
@@ -0,0 +1,29 @@
+package com.project.restaurantsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String stateProvince;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String country;
+
+}
