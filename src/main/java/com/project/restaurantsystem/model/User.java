634cd4af39d96fdabf863dd0f62e09f3684686eb
MadushanaKasilingam <madushanakasilingam003@gmail.com>
2024-08-27 20:37:52 +0530

    Commit

---
+++ src/main/java/com/project/restaurantsystem/model/User.java
@@ -0,0 +1,32 @@
+package com.project.restaurantsystem.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+}
+
+
