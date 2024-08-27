52c922af204a721de7c292e3319baf7497581464
MadushanaKasilingam <madushanakasilingam003@gmail.com>
2024-08-27 13:27:23 +0530

    Commit First

---
+++ src/test/java/com/project/restaurantsystem/RestaurantsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restaurantsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
