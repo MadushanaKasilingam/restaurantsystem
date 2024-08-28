54ef3dece4e88fd4c1866c622d90700726ccd5b3
MadushanaKasilingam <madushanakasilingam003@gmail.com>
2024-08-28 18:31:56 +0530

    Commit

---
+++ src/main/java/com/project/restaurantsystem/model/Food.java
@@ -0,0 +1,40 @@
+package com.project.restaurantsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    @ManyToOne
+    private Category foodCategory;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+
+    private boolean available;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Date createdDate;
+}
